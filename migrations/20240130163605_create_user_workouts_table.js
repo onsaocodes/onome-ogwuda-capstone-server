390a7c3d56c54cd001d4c1acaa3e8b779efc7404
Onome Ogwuda <onomeogwuda@gmail.com>
2024-01-30 17:02:34 +0000

    Set up of db

---
+++ migrations/20240130163605_create_user_workouts_table.js
@@ -0,0 +1,60 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("user", (table) => {
+      table.increments("id").primary();
+      table.string("first_name").notNullable();
+      table.string("last_name").notNullable();
+      table.string("date_of_birth").notNullable();
+      table.string("phone_number").notNullable();
+      table.string("email").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at")
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    })
+    .createTable("workouts", (table) => {
+      table.increments("id").primary();
+      table.string("date").notNullable();
+      table.string("exercise1_name").notNullable();
+      table.string("reps").notNullable();
+      table.string("load").notNullable();
+      table.string("exercise2_name").notNullable();
+      table.string("reps").notNullable();
+      table.string("load").notNullable();
+      table.string("exercise3_name").notNullable();
+      table.string("reps").notNullable();
+      table.string("load").notNullable();
+      table.string("exercise4_name").notNullable();
+      table.string("reps").notNullable();
+      table.string("load").notNullable();
+      table.string("exercise5_name").notNullable();
+      table.string("reps").notNullable();
+      table.string("load").notNullable();
+      table.string("exercise6_name").notNullable();
+      table.string("reps").notNullable();
+      table.string("load").notNullable();
+      table.integer("likes").notNullable().defaultTo(0);
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("user.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table
+        .timestamp("updated_at")
+        .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("workouts").dropTable("user");
+};
