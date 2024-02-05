54d9cbcc653be94ba8173429539d3854fb122d73
Onome Ogwuda <onomeogwuda@gmail.com>
2024-02-05 14:47:24 +0000

    update table

--- migrations/20240130163605_create_user_workouts_table.js
+++ migrations/20240130163605_create_user_workouts_table.js
@@ -11,6 +11,7 @@ exports.up = function (knex) {
       table.string("date_of_birth").notNullable();
       table.string("phone_number").notNullable();
       table.string("email").notNullable();
+      table.string("password").notNullable();
       table.timestamp("created_at").defaultTo(knex.fn.now());
       table
         .timestamp("updated_at")
@@ -58,7 +59,3 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTable("workouts").dropTable("user");
 };
-
-knex.schema.alterTable("user", function (table) {
-  table.string("password").notNullable();
-});
